7f47077fce4e9180bb3ff2a286c3bb35987859bf
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2023-10-30 15:51:50 +0300

    Repository Design Pattern implemented.

---
+++ ApiConsume/BusinessLayer/Concrete/StudentManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class StudentManager : IStudentService
+    {
+        private readonly IStudentDal _studentDal;
+
+        public StudentManager(IStudentDal studentDal)
+        {
+            _studentDal = studentDal;
+        }
+
+        public void TDelete(Student entity)
+        {
+            _studentDal.Delete(entity);
+        }
+
+        public List<Student> TGetAll()
+        {
+            return _studentDal.GetAll();
+        }
+
+        public Student TGetById(int id)
+        {
+            return _studentDal.GetById(id);
+        }
+
+        public void TInsert(Student entity)
+        {
+            _studentDal.Insert(entity);
+        }
+
+        public void TUpdate(Student entity)
+        {
+            _studentDal.Update(entity);
+        }
+    }
+}
---
+++ ApiConsume/DataAccessLayer/EntityFramework/EfStudentDal.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfStudentDal : GenericRepositoryDal<Student>, IStudentDal
+    {
+        public EfStudentDal(Context context) : base(context)
+        {
+        }
+    }
+}
